bdf92cb9c6ea84edee7e1cfc567c0c3f7accd41b
Kanishka Garg <kanishka@example.com>
2026-05-16 18:00:01 +0530

    Initial commit

---
+++ langgraph_backend.py
@@ -0,0 +1,39 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+# from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# llm = ChatOpenAI()
+llm = ChatGoogleGenerativeAI(
+    model="gemini-3-pro-preview"
+    # temperature=1.0,  # Gemini 3.0+ defaults to 1.0
+    # max_tokens=None,
+    # timeout=None,
+    # max_retries=2,
+    # other params...
+)
+
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+# Checkpointer
+checkpointer = InMemorySaver()
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
